ecdb3bacf25ed775a835fa08e0c3d3336df74b1d
Lucas <v092953@tafe.wa.edu.au>
2024-07-30 13:47:47 +0800

    player

---
+++ app/player.py
@@ -0,0 +1,18 @@
+class Player(object):
+    """
+    docstring
+    """
+    def __init__(self, _uid:str, _name:str) -> None:
+        self._uid = _uid
+        self._name = _name
+
+    @property
+    def uid(self) -> str:
+        return self._uid
+    
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    def __str__(self) -> str:
+        return f"Player name = {self._name} uid = {self._uid}"
