26978b217f7b839d0e859e7549ab5808aa2a457b
Lucas <v092953@tafe.wa.edu.au>
2024-08-04 12:20:29 +0800

    player_node

---
+++ app/player_node.py
@@ -0,0 +1,36 @@
+from player import Player
+
+
+class PlayerNode:
+    """_summary_
+    """
+    def __init__(self, player:Player) -> None:
+        self._player = player
+        self._next = None
+        self._pervious = None
+
+    @property
+    def player(self):
+        return self._player
+
+    @property
+    def next(self):
+        return self._next
+    
+    @property
+    def pervious(self):
+        return self._pervious
+    
+    @next.setter
+    def next(self):
+        self._next = next
+
+    #creating a new property variable called key
+
+    @property
+    def key(self):
+        return self._player.uid
+    
+    def __str__(self) -> str:
+        return f"{self._player} next= {self._next} pervious= {self._pervious}"
+    
