85d2f218c63617dee122a9175944bba50ab0162d
Lucas <v092953@tafe.wa.edu.au>
2024-09-01 14:03:58 +0800

    add_password

--- app/player.py
+++ app/player.py
@@ -1,9 +1,12 @@
+from argon2 import PasswordHasher
+
 class Player:
     """Holds the detail of the player
     """
     def __init__(self, _uid:str, _name:str) -> None:
         self._uid = _uid
         self._name = _name
+        self._password_hash = None
 
     @property
     def uid(self) -> str:
@@ -13,6 +16,12 @@ class Player:
     def name(self) -> str:
         return self._name
     
+    def add_password(self, word:str) -> None:
+        password = PasswordHasher()
+        self._password_hash = password.hash(word)
+    
     def __str__(self) -> str:
-        return f"Player( name = {self._name} uid = {self._uid} )"
+        #return f"Player( name = {self._name} uid = {self._uid})"
+        return f"Player( name = {self._name} uid = {self._uid} password hash = {self._password_hash})"
+    
     
