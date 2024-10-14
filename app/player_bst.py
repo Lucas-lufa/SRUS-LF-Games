d2dc8f248802fe9b317f0c08d5f59c618ca54285
Lucas <v092953@tafe.wa.edu.au>
2024-10-14 15:30:24 +0800

    save

--- app/player_bst.py
+++ app/player_bst.py
@@ -1,3 +1,6 @@
+from app.player import Player
+from app.player_bnode import PlayerBNode
+
 class PlayerBST:
     
     def __init__(self, root) -> None:
@@ -6,3 +9,38 @@ class PlayerBST:
     @property
     def _root(self):
         return self._root
+    
+    def insert(self, player:Player):
+
+        node = PlayerBNode(player)
+        root = self._root
+
+        if self.is_empty():
+            self._root = node
+            return
+        
+        PlayerBST.binary_insert(node, root)
+
+    @staticmethod
+    def binary_insert(self, node, root):
+        if (ord(node.player.name) < ord(root.player.name)):
+            if root.left is None:
+                root.left = node
+            else:
+                root = root.left
+                PlayerBST.binary_insert(node, root)
+        else:
+            if root.right is None:
+                root.right = node
+            else:
+                root = root.right
+                PlayerBST.binary_insert(node, root)
+        
+
+    def is_empty(self) -> bool:
+        """Checks to see if binary search tree is empty.
+
+        Returns:
+            bool: if the root has an object, truthy or is None, falsy.
+        """
+        return self._root is None
