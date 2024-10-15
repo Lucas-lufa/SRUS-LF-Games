a324b28c21c950944687a9e6d06ee00e4254ac75
Lucas <v092953@tafe.wa.edu.au>
2024-10-15 16:23:26 +0800

    score.setter won't accept less than zero, added docstrings to quicksort

--- app/player.py
+++ app/player.py
@@ -36,7 +36,8 @@ class Player:
     
     @score.setter
     def score(self, score:int):
-        self._score += score
+        if score > 0:
+            self._score += score
     
     def add_password(self, word:str) -> None:
         password = PasswordHasher()
@@ -53,6 +54,14 @@ class Player:
 
     @staticmethod
     def make_list(player_list):
+        """Makes a list form list from the linked list.
+
+        Args:
+            player_list (Player_List): Double linked list.
+
+        Returns:
+            list: List of player objects.
+        """
         players = []
         if player_list.head is not None:
             node = player_list._head
@@ -66,43 +75,53 @@ class Player:
     def partition(players, i, j):
         """_summary_
         https://youtu.be/7h1s2SojIRw?si=CkL1_j2xALGK_wQC&t=723
+        
+        Places the pivot into the correct place in the list, 
+        returns it's index so sub lists can be made.
 
         Args:
-            players (_type_): _description_
-            i (_type_): _description_
-            j (_type_): _description_
+            players (list): list to be mutated and sorted. 
+            i (integer): Pointer moving from the start to the end.
+            j (integer): Pointer moving from the end to the start.
 
         Returns:
-            _type_: _description_
+            integer: Pivot point moved into it's place in the list.
         """
 
         pivot = i
         
-        while i < j:
-            # low = players[i] 
-            # high = players[j] 
-            # pivotValue = players[pivot]
-            
-            # i pointer is looking for smaller than pivot. if doesn't find will increment pointer
+        while i < j:            
+        # i pointer is looking for smaller than pivot. if doesn't find will increment pointer
             while True:
                 i+=1
                 if players[pivot] > players[i]:
                     break
-
-            # j pointer looks for larger than pivot, if doesn't find will increment pointer
+        # j pointer looks for larger than pivot, if doesn't find will increment pointer
             while True:
                 j-=1
+        # If both pointers stop and the j pointer has not crossed the i pointer 
                 if players[pivot] < players[j] or players[pivot] == players[j]:
-                    if i < j:                    
+                    if i < j:
+        # Swap object at i pointer with object at j pointer
                      (players[i], players[j]) = (players[j], players[i])
                     break
-            
+        # If pointer j crosses i pointer swap object at j pointer with object at pivot pointer.
         (players[j], players[pivot]) = (players[pivot], players[j])
-
+        # index that will be used for lower and upper value of sub list.
         return j
     
     @staticmethod
     def quick_sort(players, low, high):
+        """
+        https://youtu.be/7h1s2SojIRw?si=CkL1_j2xALGK_wQC&t=723
+        
+        Sorts a list in descending order by using a quicksort algorithm.
+
+        Args:
+            players (list): list to be mutated and sorted.
+            low (int): The lower bound of list or sub list to find pivot position.
+            high (int): The upper bound of list or sub list to find pivot position.
+        """
         if (len(players[low:high]) <= 1):
             return
 
